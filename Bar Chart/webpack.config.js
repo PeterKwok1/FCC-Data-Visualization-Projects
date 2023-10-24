45c20149f25ddb4cca9cc547240dc309b4dc1562
Peter Kwok <peter.kwok94@gmail.com>
2023-10-23 22:11:34 -0400

    1:01:31

--- Bar Chart/webpack.config.js
+++ Bar Chart/webpack.config.js
@@ -1,10 +1,13 @@
+// https://stackoverflow.com/questions/72361260/why-webpack-doesnt-need-us-to-import-loaders-but-plugins-does
+// https://youtu.be/MpGLUVbqoYQ?t=3691
+
 const path = require("path")
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
     module: {
