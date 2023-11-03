a4e2de03a1825f866ac1901e982bf0ceaf355200
Peter Kwok <peter.kwok94@gmail.com>
2023-11-02 22:01:43 -0400

    webpack setup, fetch

--- Scatterplot Graph/src/index.js
+++ Scatterplot Graph/src/index.js
@@ -1,11 +1,31 @@
-// experimenting with splitchunksplugin
-// https://youtu.be/MpGLUVbqoYQ?t=5686 
-
 import "./main.scss"
 
-const body = document.querySelector("body")
-const div = document.createElement("div")
-div.textContent = "Div"
-body.append(div)
+async function fetchData() {
+    const response = await fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json")
+    const data = await response.json()
+    console.log(data)
+    console.log(data[0].Time)
+    console.log(d3.max(data, (d) => d.Time))
+    console.log(d3.min(data, (d) => d.Time))
 
-import $ from "jquery"
+    const w = 900
+    const h = 600
+    const padding = 100
+
+    const xScale = d3.scaleLinear()
+        .domain([])
+    const yScale = d3.scaleTime()
+        .domain([d3.min(data, (d) => d.Time), d3.max(data, (d) => d.Time)])
+        .range([0, 100])
+
+    console.log(yScale("39:50"))
+
+    const svg = d3.select("#container")
+        .append("svg")
+        .attr("width", "900")
+        .attr("height", "600")
+
+
+}
+
+fetchData()
--- Scatterplot Graph/webpack.common.js
+++ Scatterplot Graph/webpack.common.js
@@ -3,15 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
     entry: {
         main: "./src/index.js",
-        vendor: "./src/vendor.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            }
-        ]
     },
     plugins: [new HtmlWebpackPlugin({
         template: "./src/template.html"
