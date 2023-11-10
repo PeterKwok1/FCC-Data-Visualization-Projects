db2875d791ba35949f23568ef410c1e8ccc969b3
Peter Kwok <peter.kwok94@gmail.com>
2023-11-09 19:09:15 -0500

    webpack setup

---
+++ Heat Map/webpack.prod.js
@@ -0,0 +1,24 @@
+const path = require("path")
+const { merge } = require("webpack-merge")
+const common = require("./webpack.common.js")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = merge(common, {
+    mode: "production",
+    output: {
+        filename: "[name].[contenthash].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    ]
+})
