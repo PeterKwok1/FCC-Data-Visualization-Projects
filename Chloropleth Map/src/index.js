00a63325d38c9b40031284cc34f5718059e54541
Peter Kwok <peter.kwok94@gmail.com>
2023-11-16 18:11:44 -0500

    webpack setup, fetchdata

---
+++ Chloropleth Map/src/index.js
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=FsDyelH58F0
+
+import "./main.scss"
+
+async function fetchData() {
+    const eduResponse = await fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json")
+    const eduData = await eduResponse.json()
+
+    const topResponse = await fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
+    const topData = await topResponse.json()
+
+
+    console.log(
+        topData
+    )
+}
+
+fetchData()
+
