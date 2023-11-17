18e05a3546f88c897e28a1a18e769038c6039e16
Peter Kwok <peter.kwok94@gmail.com>
2023-11-17 15:28:13 -0500

    learning about topojson and geojson

--- Chloropleth Map/src/index.js
+++ Chloropleth Map/src/index.js
@@ -1,4 +1,6 @@
 // https://www.youtube.com/watch?v=FsDyelH58F0
+// https://forum.freecodecamp.org/t/drawing-us-map-in-d3-chloropleth/481354/4
+// https://medium.com/geekculture/advanced-map-visualization-and-cartography-with-d3-js-geojson-topojson-2de786ece0c3
 
 import "./main.scss"
 
@@ -9,9 +11,21 @@ async function fetchData() {
     const topResponse = await fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
     const topData = await topResponse.json()
 
+    const h = 600
+    const w = 900
+    const padding = 100
+
+    const svg = d3.select("#container")
+        .append("svg")
+        .attr("height", `${h}`)
+        .attr("width", `${w}`)
+        .style("background-color", "white")
+
+
 
     console.log(
-        topData
+        // topData,
+        topojson.feature(topData, topData.objects.counties)
     )
 }
 
