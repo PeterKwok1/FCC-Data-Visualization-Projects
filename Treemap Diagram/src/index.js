d74ce024fa04cda56d9eb834fda4591e31efae80
Peter Kwok <peter.kwok94@gmail.com>
2023-11-27 16:32:48 -0500

    learning d3 treemap

--- Treemap Diagram/src/index.js
+++ Treemap Diagram/src/index.js
@@ -1,15 +1,12 @@
+// https://www.youtube.com/watch?v=60HBKI5VV_4
+
 import "./main.scss"
 
-const h = 600
-const w = 900
-const padding = 100
+const h = 600, w = 900, padding = 100, legend = 100
+const svg = d3.select("#container").append("svg").attr("height", h + legend).attr("width", w).style("background-color", "white").style("box-shadow", "3px 3px 15px")
 
-const svg = d3.select("#container")
-    .append("svg")
-    .attr("height", h)
-    .attr("width", w)
-    .style("background-color", "white")
-    .style("box-shadow", "3px 3px 15px")
+const title = svg.append("text").attr("id", "title").attr("font-size", "30").attr("text-anchor", "middle").attr("transform", `translate(${w / 2}, ${padding / 2})`)
+const description = svg.append("text").attr("id", "description").attr("text-anchor", "middle").attr("transform", `translate(${w / 2}, 75)`)
 
 let sources = [
     "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json",
@@ -20,7 +17,11 @@ let sources = [
 async function fetchData(links) {
     let responses = await Promise.all(links.map(e => fetch(e)))
     let data = await Promise.all(responses.map(e => e.json()))
-    console.log(data)
+    return data
 }
+const datasets = await fetchData(sources)
+console.log(datasets)
 
-fetchData(sources)
+function appendData(data) {
+
+}
